29c0c30387d79b436800ec4733b0576af1a8152c
aryaziai <arya721@gmail.com>
2019-11-27 10:38:10 -0800

    Done.

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,16 @@
 class DogsController < ApplicationController
+
+
+    def index 
+        @dogs = Dog.all 
+    end
+
+    def show
+        @dog = Dog.find(params[:id])
+        @owners = @dog.employees.map{|owner| owner.my_owners}.join
+        @employee = Employee.find(params[:id])
+    end
+
+   
+
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,20 @@
 class Employee < ApplicationRecord
+    belongs_to :dog
+
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :office, presence: true
+    #img url should be optional
+    validates :dog_id, presence: true
+    validates :title, presence: true, uniqueness: true
+    validates :alias, presence: true#, uniqueness: true
+
+
+    def full_name #helper
+        self.first_name + " " + self.last_name
+    end
+
+    def my_owners #useful method
+        self.full_name + ". " 
+    end
 end
